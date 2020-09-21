04919c91ad036cbf129a7f4859b71010a8ca5e37
Hanssel Hurtado <hansselhurtado@gmail.com>
2020-09-21 15:50:40 -0500

    'arreglandoseeders'

--- database/seeds/Modelo_cuellos_seeder.php
+++ database/seeds/Modelo_cuellos_seeder.php
@@ -13,34 +13,42 @@ class Modelo_cuellos_seeder extends Seeder
     public function run()
     {
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 1;
         $modelo_cuello->nombre_modelo = "Liso";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 2;
         $modelo_cuello->nombre_modelo = "Lineas";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 3;
         $modelo_cuello->nombre_modelo = "Letras";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 4;
         $modelo_cuello->nombre_modelo = "Figuras";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 5;
         $modelo_cuello->nombre_modelo = "Letras, Figuras";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 6;
         $modelo_cuello->nombre_modelo = "Letras, Lienas";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 7;
         $modelo_cuello->nombre_modelo = "Letras, Figuras, Lineas";
         $modelo_cuello->save();
 
         $modelo_cuello = new Modelo_cuello();
+        $modelo_cuello->id_modelo_cuello = 8;
         $modelo_cuello->nombre_modelo = "Figuras, Lineas";
         $modelo_cuello->save();
     }
